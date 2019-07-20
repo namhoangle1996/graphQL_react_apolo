0d0d9121d73b03fbf1c1f1455fdf13af7f524b01
nam <you@example.com>
2019-07-20 11:35:22 +0700

    react and apolo binding

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema.js');
 
 const app = express();
-
+app.use(cors());
 app.use('/graphql',graphqlHTTP({
     schema, 
     graphiql : true
