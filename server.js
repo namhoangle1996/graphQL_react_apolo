63e1828608611d888d8b4bfec3de25b5dfbcc562
nam <you@example.com>
2019-07-23 10:23:12 +0700

    fix deploy

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema.js');
+const schema = require('./schema');
 const path = require('path');
-// console.log(path);
-// console.log(express.static('scss'));
 
 const app = express();
 app.use(cors());
 app.use(express.static('public'));
 app.get('*',(req,res) => {
-    res.send(path.resolve(__dirname,'public','index.html'));
+    res.sendFile(path.resolve(__dirname,'public','index.html')); 
 }); 
 
 app.use('/graphql',graphqlHTTP({
@@ -18,4 +16,5 @@ app.use('/graphql',graphqlHTTP({
     graphiql : true
 }));
 
-app.listen(4000);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT,()=> console.log(`server has started on port ${PORT}`));
