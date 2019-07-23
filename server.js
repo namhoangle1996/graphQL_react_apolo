f6b71ec38b644455ee9e985d62da16968b3f823f
nam <you@example.com>
2019-07-23 10:04:18 +0700

    make it better

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema.js');
+const path = require('path');
+// console.log(path);
+// console.log(express.static('scss'));
 
 const app = express();
 app.use(cors());
+app.use(express.static('public'));
+app.get('*',(req,res) => {
+    res.send(path.resolve(__dirname,'public','index.html'));
+}); 
+
 app.use('/graphql',graphqlHTTP({
     schema, 
     graphiql : true
